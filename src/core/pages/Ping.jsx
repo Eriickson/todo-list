1fc9fc07ca7160caa9aa398f41ebd06805bdd590
Erickson Manuel Holguín <38663299+Eriickson@users.noreply.github.com>
2021-04-15 08:18:57 -0400

    Agregando dependencias

--- src/core/pages/Ping.jsx
+++ src/core/pages/Ping.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Ping = () => {
-  return <div>sadasd</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  function onSubmit(data) {
+    console.log("Recibido");
+    console.log(data);
+  }
+  console.log(errors);
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        type="text"
+        className="border"
+        {...register("hola", {
+          minLength: { message: "Mínimo 5 caracteres", value: 5 },
+        })}
+      />
+    </form>
+  );
 };
 
 export default Ping;
